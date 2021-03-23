1e5fcb5de2ef695dfe0c40448adf3cd5f415d7ee
AJ <alexanderjessediaz@gmail.com>
2021-03-23 16:04:54 -0600

    create update for posts, connect it to backend, prop drill from App to Post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import moment from 'moment';
 
 import useStyles from './styles';
 
-const Post = ({post}) => {
+const Post = ({post, setCurrentId }) => {
     const classes = useStyles();
 
     
@@ -20,7 +20,10 @@ const Post = ({post}) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color:'white'}} size="small" onClick={() => {}}>
+                <Button 
+                    style={{color:'white'}} 
+                    size="small" 
+                    onClick={() => setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
