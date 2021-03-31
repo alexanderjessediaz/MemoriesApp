fd928ac8f127404b4aea4641d733ca501a0a7b18
AJ <alexanderjessediaz@gmail.com>
2021-03-31 14:57:11 -0600

    build out Input.js for cleaner Input fields in auth. Build out auth.js add styling

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,42 @@
 import React from 'react'
-
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from "@material-ui/core"
+import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+import Input from './Input'
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+
+    const isSignup = false;
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up': "Sign In"}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+
+                                <>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} half/>
+                                </>
+                            )
+                        }
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
